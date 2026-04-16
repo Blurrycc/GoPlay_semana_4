251812933a2c7c33a4bc4d0e68e6ac08fb4a05e8
BastianC <bas.carrilloc@gmail.com>
2026-04-16 15:06:37 -0400

    Vistas y rutas
    Creamos las vistas correspondientes a cada pagina y luego las demás rutas, para luego actualizar los enlaces

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from . import views # Importamos las vistas que acabamos de crear
+from . import views # Importamos las vistas creadas en tienda/views.py
 
 urlpatterns = [
-    # Si la ruta está vacía (''), carga la vista 'index'
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), # La ruta vacía es el inicio
+    path('login/', views.login, name='login'),
+    path('registro/', views.registro, name='registro'),
+    path('recuperar/', views.recuperar, name='recuperar'),
+    path('perfil/', views.perfil, name='perfil'),
+    path('carrito/', views.carrito, name='carrito'),
+    path('mantenedor/', views.mantenedor_productos, name='mantenedor_productos'),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 
+# Vista de la página principal
 def index(request):
-    # Esta función toma la petición del usuario y le devuelve el archivo index.html
     return render(request, 'tienda/index.html')
+
+# Vistas de Usuario
+def login(request):
+    return render(request, 'tienda/login.html')
+
+def registro(request):
+    return render(request, 'tienda/registro.html')
+
+def recuperar(request):
+    return render(request, 'tienda/recuperar.html')
+
+def perfil(request):
+    return render(request, 'tienda/perfil.html')
+
+# Vistas de Tienda
+def carrito(request):
+    return render(request, 'tienda/carrito.html')
+
+def mantenedor_productos(request):
+    return render(request, 'tienda/mantenedor_productos.html')
