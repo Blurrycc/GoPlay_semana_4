1d1877d162835bef08fb69c7bc7968cd27bb51f3
BastianC <bas.carrilloc@gmail.com>
2026-04-18 11:09:38 -0400

    Migracion paginas catalogo videojuegos

--- tienda/views.py
+++ tienda/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 # Vista de la página principal
 def index(request):
-    return render(request, 'tienda/index.html')
+    # 1. Creamos una variable y una lista de datos en Python
+    nombre_gamer = "GamerX_99"
+    lista_juegos = ["Super Mario Bros. Wonder", "Zelda: Tears of the Kingdom", "Monster Hunter Wilds"]
+    
+    # 2. Empaquetamos esos datos en un 'contexto' (diccionario)
+    contexto = {
+        "usuario": nombre_gamer,
+        "juegos": lista_juegos
+    }
+    
+    # 3. Enviamos el contexto al HTML
+    return render(request, 'tienda/index.html', contexto)
 
 # Vistas de Usuario
 def login(request):
